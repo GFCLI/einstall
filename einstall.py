12b2bef1d744ef6d68a469a0118e0cfefaf831d3
David Tesler <dtesler@users.noreply.github.com>
2014-12-08 19:03:36 -0800

    Update einstall.py

--- einstall.py
+++ einstall.py
@@ -2,9 +2,9 @@ from py_compile import compile
 import os
 
 def main(args):
-    args = args.split("#/")
-    pkname = args[0]
-    os.system("git clone https://github.com/" + pkname + ".git")
-    os.system("copy " + pkname + "/" + pkname + ".py " + pkname + ".py")
-    compile(pkname + ".py")
-    os.system("DEL " + pkname + ".py;DEL " + pkname)
+    pkname = args
+    if raw_input("You are about to install " + pkname + " which originates from an unverified repository.  Are you sure you want to continue? (yN)"):
+        os.system("git clone https://github.com/" + pkname + ".git")
+        os.system("copy " + pkname + "/" + pkname + ".py " + pkname + ".py")
+        compile(pkname + ".py")
+        os.system("DEL " + pkname + ".py;rd /s /q " + pkname)
